8cce7938a554c5b9df190ed29a221b2ba443949b
RenanFerreira <ferreirarenan@live.com>
2018-08-20 10:21:36 -0300

    PokemonFinalAPI

---
+++ app/src/main/java/br/com/opet/tds/pokeapiapp/Model/Pokemon.java
@@ -0,0 +1,127 @@
+package br.com.opet.tds.pokeapiapp.Model;
+
+
+import com.google.gson.annotations.SerializedName;
+import android.net.Uri;
+import java.util.List;
+import java.net.URI;
+import java.net.URL;
+import java.util.List;
+/**
+ * Created by Renan on 12/08/2018.
+ */
+
+public class Pokemon {
+
+    @SerializedName("id")
+    private long ID;
+    private String name;
+    private int height;
+    private int weight;
+    private List<Types> types;
+
+    private Sprites sprites;
+    private String Linksprite;
+
+    public Pokemon() {
+    }
+
+    public Sprites getSprites() {
+        return sprites;
+    }
+
+    public void setSprites(Sprites sprites) {
+        this.sprites = sprites;
+    }
+
+    public String getSpriteURL() {
+        String linksprite = getSprites().getFront_default();
+        Linksprite = linksprite;
+        return linksprite;
+    }
+
+    public void setSpriteURL(String spriteURL) {
+        this.Linksprite = spriteURL;
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public List<Types> getTypes() {
+        return types;
+    }
+
+    public void setTypes(List<Types> types) {
+        this.types = types;
+    }
+
+    public class Types{
+        private Type type;
+
+        public Types() {
+        }
+
+        public Type getType() {
+            return type;
+        }
+
+        public void setType(Type type) {
+            this.type = type;
+        }
+
+        public class Type{
+            private String name;
+
+            public Type() {
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+        }
+    }
+    public class Sprites {
+        private String front_default;
+
+        public String getFront_default(){ return front_default;}
+
+        public void setFront_default(String front_default) { this.front_default = front_default;}
+
+    }
+}
+
+
+
